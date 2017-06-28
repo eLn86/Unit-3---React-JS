e9c16c237229ea0aa10417eea007b1fc041aae20
Elnathan Erh <elnathan.erh@gmail.com>
2017-06-28 11:51:02 +0800

    added Cart/Create/GrandTotal/ProductRow added css for Header

--- react101/src/components/App/App.js
+++ react101/src/components/App/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import SubTitle from '../SubTitle/SubTitle'
+
+// Import components
+import Header from '../Header/Header'
+import Cart from '../Cart/Cart'
+import ProductRow from '../ProductRow/ProductRow'
+import Create from '../Create/Create'
+import GrandTotal from '../GrandTotal/GrandTotal'
+
+// Import static assets
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,15 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to WDI Singapore</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SubTitle title="Hello from SubTitle" color="red"/>
-        <SubTitle title="Hello from WDI Singapore" color="blue"/>
+        <Header title="My Amazing Cart"/>
       </div>
     );
   }
---
+++ react101/src/components/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class Cart extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+Cart.propTypes = {
+};
